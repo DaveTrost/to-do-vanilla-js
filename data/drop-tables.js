b1b37c38dcbfc30cc9a4bad58fda8a7089373abd
Dave Trost <davetrost@gmail.com>
2019-08-28 09:29:44 -0700

    refactor database setup with Marty's new client.js

--- data/drop-tables.js
+++ data/drop-tables.js
@@ -1,11 +1,8 @@
 const client = require('../lib/client');
 
-client.connect()
-    .then(() => {
-        return client.query(`
-            DROP TABLE IF EXISTS tasks;
-        `);
-    })
+client.query(`
+    DROP TABLE IF EXISTS tasks;
+`)
     .then(
         () => console.log('drop tables complete'),
         err => console.log(err)
