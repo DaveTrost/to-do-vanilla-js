33106a34a49c03e57f5136cb7fe03f21241f79c7
Dave Trost <davetrost@gmail.com>
2019-08-28 09:38:11 -0700

    add users table (create + drop)

--- data/create-tables.js
+++ data/create-tables.js
@@ -1,6 +1,12 @@
 const client = require('../lib/client');
 
 client.query(`
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY NOT NULL,
+        email VARCHAR(256) NOT NULL,
+        hash VARCHAR(256) NOT NULL,
+        display_name VARCHAR(256) NOT NULL
+    );
     CREATE TABLE IF NOT EXISTS tasks (
         id SERIAL PRIMARY KEY NOT NULL,
         task VARCHAR(256) NOT NULL,
--- data/drop-tables.js
+++ data/drop-tables.js
@@ -1,6 +1,7 @@
 const client = require('../lib/client');
 
 client.query(`
+    DROP TABLE IF EXISTS users;
     DROP TABLE IF EXISTS tasks;
 `)
     .then(
